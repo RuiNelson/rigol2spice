bfd4cc3a6bbb781ec83a1a7e3b9eaa8cd6075e3c
Rui Carneiro <RuiCarneiro@users.noreply.github.com>
2022-04-22 15:34:15 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
-        .package(url: "https://github.com/RuiCarneiro/SwiftEngineeringNumberFormatter", from: "1.1.0")
+        .package(url: "https://github.com/RuiCarneiro/SwiftEngineeringNumberFormatter", from: "1.1.0"),
     ],
     targets: [
         .executableTarget(
             name: "rigol2spice",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "SwiftEngineeringNumberFormatter", package: "SwiftEngineeringNumberFormatter")
+                .product(name: "SwiftEngineeringNumberFormatter", package: "SwiftEngineeringNumberFormatter"),
             ]
         ),
         .testTarget(
