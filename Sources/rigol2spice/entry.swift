4f29ffb5357534e6989c2a225172687b07909c6e
Rui Carneiro <RuiCarneiro@users.noreply.github.com>
2022-04-23 13:19:49 +0100

    removeDc is a flag not an option

--- Sources/rigol2spice/entry.swift
+++ Sources/rigol2spice/entry.swift
@@ -40,7 +40,7 @@ struct rigol2spice: ParsableCommand {
     @Option(name: .shortAndLong, help: "The label of the channel to be processed")
     var channel: String = "CH1"
     
-    @Option(name: [.customLong("dc", withSingleDash: true), .customLong("remove-dc")], help: "Remove DC component")
+    @Flag(name: [.customLong("dc", withSingleDash: true), .customLong("remove-dc")], help: "Remove DC component")
     var removeDc: Bool = false
     
     @Option(name: .shortAndLong, help: "Offset factor for signal")
