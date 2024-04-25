b5c2d1f8acfd7f78cedfce54a1876b54043ab60e
Rui Nelson <7100905+RuiNelson@users.noreply.github.com>
2024-04-25 13:42:20 +0100

    Update SwiftEngineeringNumberFormatter version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
         .package(url: "https://github.com/RuiNelson/Progress.swift", from: "0.5.0"),
-        .package(url: "https://github.com/RuiNelson/SwiftEngineeringNumberFormatter", from: "1.1.0"),
+        .package(url: "https://github.com/RuiNelson/SwiftEngineeringNumberFormatter", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
